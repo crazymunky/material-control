253945a4e6e3e127a2a2ff4eb6bdb92aa76a837b
MunKy <maxi.techerag@gmail.com>
2015-07-16 14:35:04 -0300

    First steps

---
+++ app.js
@@ -0,0 +1,8 @@
+(function(){
+    'use strict';
+
+    angular.module('backendApp', ['ngMaterial'])
+        .controller('ListController', ['$scope', '$http', function($scope,$http){
+
+        }]);
+})();
