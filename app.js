adf223d19cab8f0c6e500288fe349c45a64db6d4
MunKy <maxi.techerag@gmail.com>
2015-07-17 22:24:29 -0300

    Added dependencies

--- app.js
+++ app.js
@@ -2,7 +2,23 @@
     'use strict';
 
     angular.module('backendApp', ['ngMaterial'])
-        .controller('ListController', ['$scope', '$http', function($scope,$http){
-
+        .config(function($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('light-blue', {
+                    'default': '400', // by default use shade 400 from the pink palette for primary intentions
+                    'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+                    'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+                    'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+                })
+                // If you specify less than all of the keys, it will inherit from the
+                // default shades
+                .accentPalette('amber', {
+                    'default': '200' // use shade 200 for default, and keep all other shades the same
+                })
+        })
+        .controller('ListController', ['$scope', '$http', '$mdSidenav', function($scope,$http, $mdSidenav){
+            $scope.openLeftMenu = function() {
+                $mdSidenav('side-nav').toggle();
+            };
         }]);
 })();
