10093fb0cc8643d185cf666434d1c4c3ca8dc838
MunKy <maxi.techerag@gmail.com>
2015-07-29 19:24:01 -0300

    asdfasdf

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
 
-    angular.module('backendApp', ['ngMaterial'])
+    angular.module('backendApp', ['ngMaterial', 'ngResource', 'backendApp.controllers', 'backendApp.services'])
         .config(function($mdThemingProvider) {
             $mdThemingProvider.theme('default')
                 .primaryPalette('light-blue', {
@@ -15,10 +15,18 @@
                 .accentPalette('amber', {
                     'default': '200' // use shade 200 for default, and keep all other shades the same
                 })
-        })
-        .controller('ListController', ['$scope', '$http', '$mdSidenav', function($scope,$http, $mdSidenav){
+        });
+
+    angular.module('backendApp.services',[]).factory('Noticia', function($resource){
+        return $resource('http://192.168.235.153/musica_para_tus_oidos/public/api/noticias/:id');
+    });
+
+    angular.module('backendApp.controllers',[]).controller( 'ListController',['$scope', '$http', '$mdSidenav',
+        function($scope,$http, $mdSidenav){
+            $scope.noticias = Noticia.query();
             $scope.openLeftMenu = function() {
                 $mdSidenav('side-nav').toggle();
             };
         }]);
+
 })();
